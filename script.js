1714f055aa83da495822ed0a1e0bf1f3b5ce3082
sani serge <sergyth@gmail.com>
2026-02-04 13:19:05 +0100

    realiser une première version de l'affichage des cartes couvertes au chargement de la page. J'ai réalisé aussi une fonction shuffleCards pour melanger les cartes

--- script.js
+++ script.js
@@ -1,13 +1,60 @@
 const cardsWrapper = document.getElementById("cards_wrapper");
-const gameSet = ["❤️", "💕", "🙌", "😘", " 🤣", " 🤷‍♂️", "✌️","😎", "🍟", "👨"];
+const btnJeu = document.getElementById("btnJeu")
+const gameSet = ["❤️", "💕", "🙌", "😘", " 🤣", " 🤷‍♂️", "✌️", "😎", "🍟", "👨"];
 
 
+document.addEventListener("DOMContentLoaded", loadCards())
 
+function createCard(nbreCarte) {
+    for (i = 0; i < nbreCarte; i++) {
+        const cardContainer = cardsWrapper.appendChild(document.createElement("div"))
+        const cardFront = cardContainer.appendChild(document.createElement("div"));
+        const cardBack = cardContainer.appendChild(document.createElement("div"));
+        cardFront.classList.add("card", "hide")
+        cardBack.classList.add("card")
+    }
 
-function createCard(){
-    const card = cardsWrapper.appendChild(document.createElement("div"));
+}
+
+function loadCards() { 
+    //pour le moment il charge juste les back cards vides
+    createCard(gameSet.length * 2)
     
 }
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function shuffleCards() {
+    const setToShuffle = []
+    const shuffledSet = []
+    const sortedNumbers = []
+    gameSet.forEach(item => setToShuffle.push(item, item))
+    for (i = 0; i < setToShuffle.length; i++) {
+        let random = getRandomInt(setToShuffle.length)
+        while (sortedNumbers.includes(random)) {
+            random = getRandomInt(setToShuffle.length)
+        }
+        sortedNumbers.push(random)
+        shuffledSet[random] = setToShuffle[i]
+    }
+
+
+
+    console.log(`"setToShuffle: >> " ${setToShuffle}`);
+    console.log(`"sortedNumbers: >> " ${sortedNumbers}`);
+    console.log(`"shuffleSet: >> " ${shuffledSet}`);
+
+}
+
+
+
+function play() {
+    btnJeu.addEventListener("click", shuffleCards)
+}
+
+
 
 
 
